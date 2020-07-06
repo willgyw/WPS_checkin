22d3744bdd5eeb2042de62a7951c1649ad550468
Guo Yingwei <41852697+willgyw@users.noreply.github.com>
2020-07-06 15:03:45 +0800

    Update main.py

--- main.py
+++ main.py
@@ -46,9 +46,8 @@ if __name__ == '__main__':
     root_dir = os.path.split(os.path.realpath(__file__))[0]
     print(root_dir)
     f = open('./data.json', 'r', encoding="utf-8")
-    for line in f:
-        print(line)
     data = json.load(f)
+    print(data)
     f.close()
 
     _wps_checkin(data)
